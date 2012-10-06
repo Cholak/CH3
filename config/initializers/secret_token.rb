d6002c259e2c462bb6363df34624621f5858e50f
Cholak <yellowfluke@gmail.com>
2012-10-06 13:24:07 +0200

    Depot initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ch3::Application.config.secret_token = 'f7fbcba1160a8363a8410bdc37abb580be703f5dcddf167fb43b6ba641efec896b3b63ae00382e54256e297130937dd358dac8c21c7250dfea7377fc52cdbdaa'
